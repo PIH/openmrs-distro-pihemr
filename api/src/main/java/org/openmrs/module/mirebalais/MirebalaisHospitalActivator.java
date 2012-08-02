ae11527461d6883ada3e3a8f74435128d06f6bc3
djazayeri <jazayeri@alum.mit.edu>
2012-08-02 15:32:41 -0400

    Included a MDS package, with a failing unit test trying to load that in the activator.

--- api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
+++ api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
@@ -14,9 +14,22 @@
 package org.openmrs.module.mirebalais;
 
 
-import org.apache.commons.logging.Log; 
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.ModuleActivator;
+import org.openmrs.module.metadatasharing.ImportConfig;
+import org.openmrs.module.metadatasharing.ImportMode;
+import org.openmrs.module.metadatasharing.ImportedPackage;
+import org.openmrs.module.metadatasharing.MetadataSharing;
+import org.openmrs.module.metadatasharing.api.MetadataSharingService;
+import org.openmrs.module.metadatasharing.wrapper.PackageImporter;
 
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
@@ -24,6 +37,12 @@ import org.openmrs.module.ModuleActivator;
 public class MirebalaisHospitalActivator implements ModuleActivator {
 	
 	protected Log log = LogFactory.getLog(getClass());
+	
+	Map<String, String> currentMetadataVersions = new LinkedHashMap<String, String>();
+	
+	public MirebalaisHospitalActivator() {
+		currentMetadataVersions.put("93fb4477-843f-4184-9486-1cb879a302da", "Mirebalais_Core_Metadata-1.zip");
+	}
 		
 	/**
 	 * @see ModuleActivator#willRefreshContext()
@@ -50,6 +69,7 @@ public class MirebalaisHospitalActivator implements ModuleActivator {
 	 * @see ModuleActivator#started()
 	 */
 	public void started() {
+		installMetadataPackages();
 		log.info("Mirebalais Hospital Module started");
 	}
 	
@@ -66,5 +86,47 @@ public class MirebalaisHospitalActivator implements ModuleActivator {
 	public void stopped() {
 		log.info("Mirebalais Hospital Module stopped");
 	}
-		
+	
+    private void installMetadataPackages() {
+    	for (Map.Entry<String, String> e : currentMetadataVersions.entrySet()) {
+    		installMetadataPackageIfNecessary(e.getKey(), e.getValue());
+    	}
+    }
+    
+    /**
+     * Checks whether the given version of the MDS package has been installed yet, and if not, install it
+     * 
+     * @param groupUuid
+     * @param filename should end in "-${versionNumber}.zip"
+     * @return whether any changes were made to the db
+     * @throws IOException 
+     */
+    private boolean installMetadataPackageIfNecessary(String groupUuid, String filename) {
+    	try {
+			Matcher matcher = Pattern.compile("\\w+-(\\d+).zip").matcher(filename);
+			if (!matcher.matches())
+				throw new RuntimeException("Filename must match PackageNameWithNoSpaces-v1.zip");
+			Integer version = Integer.valueOf(matcher.group(1));
+			
+			ImportedPackage installed = Context.getService(MetadataSharingService.class).getImportedPackageByGroup(groupUuid);
+			if (installed != null && installed.getVersion() >= version) {
+				log.info("Metadata package " + filename + " is already installed with version " + installed.getVersion());
+				return false;
+			}
+			
+			if (getClass().getClassLoader().getResource(filename) == null) {
+				throw new RuntimeException("Cannot find " + filename + " for group " + groupUuid + ". Make sure it's in api/src/main/resources");
+			}
+			
+			PackageImporter metadataImporter = MetadataSharing.getInstance().newPackageImporter();
+			metadataImporter.setImportConfig(ImportConfig.valueOf(ImportMode.PARENT_AND_CHILD));
+			metadataImporter.loadSerializedPackageStream(getClass().getClassLoader().getResourceAsStream(filename));
+			metadataImporter.importPackage();
+			return true;
+    	} catch (Exception ex) {
+    		log.error("Failed to install metadata package " + filename, ex);
+    		return false;
+    	}
+    }
+    
 }
