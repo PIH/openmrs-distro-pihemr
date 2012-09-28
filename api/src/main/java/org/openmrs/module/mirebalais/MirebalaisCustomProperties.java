2a92787a8a9a2ad0d4b7dae16f0babb04ae3ea4b
Mário <mariossareias@gmail.com>
2012-09-28 15:30:42 -0300

    Changing the URL default to doesn't use batchSize in the URL, because we'll use as a parameter in the POST request. Also changing the environment variable to uppercase

--- api/src/main/java/org/openmrs/module/mirebalais/MirebalaisCustomProperties.java
+++ api/src/main/java/org/openmrs/module/mirebalais/MirebalaisCustomProperties.java
@@ -25,8 +25,8 @@ import static org.openmrs.module.mirebalais.MirebalaisConstants.REMOTE_ZL_IDENTI
 import static org.openmrs.module.mirebalais.MirebalaisConstants.REMOTE_ZL_IDENTIFIER_SOURCE_USERNAME;
 
 public class MirebalaisCustomProperties {
-	
-	public static final String MIREBALAIS_CUSTOM_PROPERTIES_FILE = "mirebalais_custom_properties_file";
+
+	public static final String MIREBALAIS_CUSTOM_PROPERTIES_FILE = "MIREBALAIS_CUSTOM_PROPERTIES_FILE";
 	
 	public static final String REMOTE_ZLIDENTIFIER_URL = "remote_zlidentifier_url";
 	
