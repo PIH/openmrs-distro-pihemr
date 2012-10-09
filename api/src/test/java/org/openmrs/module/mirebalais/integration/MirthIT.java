8b03be0cf59e01a51f759d769d092c7ec60bb416
mogoodrich <mgoodrich@pih.org>
2012-10-09 15:38:38 -0400

    Added new locations pacakge with Mirebalais tagged as "Medical Record Location"

--- api/src/test/java/org/openmrs/module/mirebalais/integration/MirthIT.java
+++ api/src/test/java/org/openmrs/module/mirebalais/integration/MirthIT.java
@@ -178,8 +178,10 @@ public class MirthIT extends BaseModuleContextSensitiveTest {
 		
 		// now create and save the order for this patient
 		Order order = new Order();
-		order.setOrderType(Context.getOrderService().getOrderTypeByUuid(
-		    Context.getAdministrationService().getGlobalProperty(PacsIntegrationGlobalProperties.RADIOLOGY_ORDER_TYPE_UUID))); // TODO: change this based on how we actually end up doing orders
+		order
+		        .setOrderType(Context.getOrderService().getOrderTypeByUuid(
+		            Context.getAdministrationService().getGlobalProperty(
+		                PacsIntegrationGlobalProperties.RADIOLOGY_ORDER_TYPE_UUID))); // TODO: change this based on how we actually end up doing orders
 		order.setPatient(patient);
 		order.setConcept(Context.getConceptService().getConceptByName("X-RAY CHEST")); // TODO: replace this with an actual radiology concept
 		order.setAccessionNumber("ACCESSION NUMBER");
