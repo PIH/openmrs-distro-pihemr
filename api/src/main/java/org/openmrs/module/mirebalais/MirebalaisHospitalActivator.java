4381b0f1457c221009cee7ec3710e712f20cd3c4
mogoodrich <mgoodrich@pih.org>
2012-11-26 14:42:29 -0500

    removed reference to AUTOMATICALLY_GENERATE_DOSSIER_NUMBER global property, since it has been removed

--- api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
+++ api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
@@ -435,7 +435,6 @@ public class MirebalaisHospitalActivator implements ModuleActivator {
 		setExistingGlobalProperty(PatientRegistrationGlobalProperties.ID_CARD_LABEL_TEXT, "Zanmi Lasante Patient ID Card");
 		setExistingGlobalProperty(PatientRegistrationGlobalProperties.ICD10_CONCEPT_SOURCE, "ICD-10");
 		setExistingGlobalProperty(PatientRegistrationGlobalProperties.BIRTH_YEAR_INTERVAL, "1");
-		setExistingGlobalProperty(PatientRegistrationGlobalProperties.AUTOMATICALLY_GENERATE_DOSSIER_NUMBER, "true");
 		setExistingGlobalProperty(PatientRegistrationGlobalProperties.MEDICAL_RECORD_LOCATION,
 		    "71c99f93-bc0c-4a44-b573-a7ac096ff636");
 		
