f2a7c73f920f3399b4a80c4d2adb385ddaa52cac
mogoodrich <mgoodrich@pih.org>
2012-11-30 11:04:20 -0500

    auto-formatting changes

--- omod/src/main/java/org/openmrs/module/mirebalais/page/controller/StandardPageController.java
+++ omod/src/main/java/org/openmrs/module/mirebalais/page/controller/StandardPageController.java
@@ -1,22 +1,18 @@
 package org.openmrs.module.mirebalais.page.controller;
 
-
 import org.openmrs.Location;
 import org.openmrs.api.LocationService;
-import org.openmrs.api.context.Context;
 import org.openmrs.module.emr.EmrContext;
 import org.openmrs.ui.framework.annotation.SpringBean;
-import org.openmrs.util.PrivilegeConstants;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public class StandardPageController {
-
-    public String post(@RequestParam(value = "locationId", required = false) Integer locationId,
-                       @SpringBean("locationService") LocationService locationService,
-                       EmrContext context){
-        Location location = locationService.getLocation(locationId);
-        context.setSessionLocation(location);
-        return null;
-    }
-
+	
+	public String post(@RequestParam(value = "locationId", required = false) Integer locationId,
+	        @SpringBean("locationService") LocationService locationService, EmrContext context) {
+		Location location = locationService.getLocation(locationId);
+		context.setSessionLocation(location);
+		return null;
+	}
+	
 }
