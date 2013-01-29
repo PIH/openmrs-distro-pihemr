d0f869fc7e06523ccc9e43fe023ee7d4dcdd3e45
djazayeri <jazayeri@alum.mit.edu>
2013-01-29 12:31:53 -0500

    #117 - Granting privilege to clinical user

--- api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
+++ api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
@@ -82,8 +82,8 @@ public class MirebalaisHospitalActivator implements ModuleActivator {
 		
 		currentMetadataVersions.add(new MetadataPackageConfig("Mirebalais_Hospital_locations",
 		        "32d52080-13fa-413e-a23e-6ff9a23c7a69", 10, ImportMode.PARENT_AND_CHILD));
-		currentMetadataVersions.add(new MetadataPackageConfig("Roles_and_privileges",
-		        "f12f5fb8-80a8-40d0-a20e-24af2642ce4c", 7, ImportMode.MIRROR));
+		currentMetadataVersions.add(new MetadataPackageConfig("HUM_Roles_and_Privileges",
+		        "f12f5fb8-80a8-40d0-a20e-24af2642ce4c", 8, ImportMode.MIRROR));
 		currentMetadataVersions.add(new MetadataPackageConfig("HUM_Metadata",
 		        "fa25ad0c-66cc-4715-8464-58570f7b5132", 18, ImportMode.MIRROR));
 		currentMetadataVersions.add(new MetadataPackageConfig("PACS_Integration", "be592ba7-1fa2-4a71-a147-3c828e67e901", 1,
