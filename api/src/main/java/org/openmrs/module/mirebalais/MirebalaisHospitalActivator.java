fb600eb4e21605b1be21af92fe15b2262cc28146
mogoodrich <mgoodrich@pih.org>
2013-04-05 16:32:18 -0400

    added configuration of unknown location global property

--- api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
+++ api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
@@ -356,6 +356,8 @@ public class MirebalaisHospitalActivator implements ModuleActivator {
 		setExistingGlobalProperty(EmrConstants.PAYMENT_CONSTRUCT_CONCEPT, "7a6330f1-9503-465c-8d63-82e1ad914b47");
 
         setExistingGlobalProperty(EmrConstants.GP_DIAGNOSIS_SET_OF_SETS, "8fcd0b0c-f977-4a66-a1b5-ad7ce68e6770");
+
+        setExistingGlobalProperty(EmrConstants.GP_UNKNOWN_LOCATION, "8d6c993e-c2cc-11de-8d13-0010c6dffd0f");
 	}
 
     private void setupRadiologyGlobalProperties() {
