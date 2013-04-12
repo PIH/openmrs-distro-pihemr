b67b674a82bf82fb0a77802a348e8344fc2799e7
lnball <eball@pih.org>
2013-04-12 11:41:24 -0400

    Added Radiology study and report constructs.  Added monthly notifiable diseases and moved all the notifiable diseases into one set.  Still didn't add the emergency diagnoses.

--- api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
+++ api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
@@ -97,11 +97,11 @@ public class MirebalaisHospitalActivator implements ModuleActivator {
         currentMetadataVersions.add(new MetadataPackageConfig("HUM_Roles_and_Privileges",
                 "f12f5fb8-80a8-40d0-a20e-24af2642ce4c", 18, ImportMode.MIRROR));
 		currentMetadataVersions.add(new MetadataPackageConfig("HUM_Metadata",
-		        "fa25ad0c-66cc-4715-8464-58570f7b5132", 28, ImportMode.MIRROR));
+		        "fa25ad0c-66cc-4715-8464-58570f7b5132", 31, ImportMode.MIRROR));
 		currentMetadataVersions.add(new MetadataPackageConfig("PACS_Integration",
                 "be592ba7-1fa2-4a71-a147-3c828e67e901", 1,  ImportMode.MIRROR));
 		currentMetadataVersions.add(new MetadataPackageConfig("HUM_Clinical_Concepts",
-		        "7003f131-7a15-4292-9513-c9fe52a73235", 25, ImportMode.MIRROR));
+		        "7003f131-7a15-4292-9513-c9fe52a73235", 26, ImportMode.MIRROR));
         currentMetadataVersions.add(new MetadataPackageConfig("HUM_Surgery",
                 "a253327a-e222-4569-92af-847278bf0169", 7, ImportMode.MIRROR));
         currentMetadataVersions.add(new MetadataPackageConfig("HUM_Provider_Roles",
