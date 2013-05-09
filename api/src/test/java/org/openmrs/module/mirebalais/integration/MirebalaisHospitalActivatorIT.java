ed11592fef4125318c4eab797e48ce18c6ceb4b5
djazayeri <jazayeri@alum.mit.edu>
2013-05-08 18:30:46 -0700

    Mirebalais Metadata module - #542 executing a dataset I'd forgotten in the integration test

--- api/src/test/java/org/openmrs/module/mirebalais/integration/MirebalaisHospitalActivatorIT.java
+++ api/src/test/java/org/openmrs/module/mirebalais/integration/MirebalaisHospitalActivatorIT.java
@@ -24,66 +24,67 @@ import static org.junit.Assert.assertNotNull;
 
 @SkipBaseSetup
 public class MirebalaisHospitalActivatorIT extends BaseModuleContextSensitiveTest {
-	
-	private MirebalaisHospitalActivator activator;
-	
-	private MirebalaisCustomProperties customProperties;
-	
-	@Before
-	public void beforeEachTest() throws Exception {
-		initializeInMemoryDatabase();
-		executeDataSet("requiredDataTestDataset.xml");
-		executeDataSet("globalPropertiesTestDataset.xml");
-		authenticate();
-		activator = new MirebalaisHospitalActivator();
-		activator.started();
-		customProperties = new MirebalaisCustomProperties();
-	}
-	
-	@AfterClass
-	public static void tearDown() {
-		runtimeProperties = null;
-	}
-	
-	@Test
-	@DirtiesContext
-	public void testThatActivatorDoesAllSetup() throws Exception {
-		MirebalaisHospitalService service = Context.getService(MirebalaisHospitalService.class);
-		
-		IdentifierPool localZlIdentifierPool = service.getLocalZlIdentifierPool();
-		RemoteIdentifierSource remoteZlIdentifierSource = service.getRemoteZlIdentifierSource();
-		SequentialIdentifierGenerator dossierSequenceGenerator = service.getDossierSequenceGenerator();
-		
-		PatientIdentifierType zlIdentifierType = Context.getPatientService().getPatientIdentifierTypeByUuid(
-		    MirebalaisConstants.ZL_IDENTIFIER_TYPE_UUID);
-		
-		PatientIdentifierType dossierNumberIdentifierType = Context.getPatientService().getPatientIdentifierTypeByUuid(
-		    MirebalaisConstants.DOSSIER_NUMBER_IDENTIFIER_TYPE_UUID);
-		
-		AutoGenerationOption autoGenerationOption = Context.getService(IdentifierSourceService.class)
-		        .getAutoGenerationOption(zlIdentifierType);
-		
-		assertEquals(MirebalaisConstants.ZL_IDENTIFIER_TYPE_UUID, zlIdentifierType.getUuid());
-		assertEquals(zlIdentifierType, autoGenerationOption.getIdentifierType());
-		assertEquals(localZlIdentifierPool, autoGenerationOption.getSource());
-		
-		assertEquals(MirebalaisConstants.LOCAL_ZL_IDENTIFIER_POOL_UUID, localZlIdentifierPool.getUuid());
-		assertEquals(MirebalaisConstants.LOCAL_ZL_IDENTIFIER_POOL_BATCH_SIZE, localZlIdentifierPool.getBatchSize());
-		assertEquals(MirebalaisConstants.LOCAL_ZL_IDENTIFIER_POOL_MIN_POOL_SIZE, localZlIdentifierPool.getMinPoolSize());
-		
-		assertEquals(MirebalaisConstants.REMOTE_ZL_IDENTIFIER_SOURCE_UUID, remoteZlIdentifierSource.getUuid());
-		assertEquals(customProperties.getRemoteZlIdentifierSourceUrl(), remoteZlIdentifierSource.getUrl());
-		assertEquals(customProperties.getRemoteZlIdentifierSourceUsername(), remoteZlIdentifierSource.getUser());
-		assertEquals(customProperties.getRemoteZlIdentifierSourcePassword(), remoteZlIdentifierSource.getPassword());
-		
-		assertEquals("A", dossierSequenceGenerator.getPrefix());
-		assertEquals(new Integer(7), dossierSequenceGenerator.getLength());
-		assertEquals("0123456789", dossierSequenceGenerator.getBaseCharacterSet());
-		assertEquals("000001", dossierSequenceGenerator.getFirstIdentifierBase());
-		assertEquals(MirebalaisConstants.DOSSIER_NUMBER_ZL_IDENTIFIER_SOURCE_UUID, dossierSequenceGenerator.getUuid());
-		assertEquals(dossierNumberIdentifierType, dossierSequenceGenerator.getIdentifierType());
+
+    private MirebalaisHospitalActivator activator;
+
+    private MirebalaisCustomProperties customProperties;
+
+    @Before
+    public void beforeEachTest() throws Exception {
+        initializeInMemoryDatabase();
+        executeDataSet("requiredDataTestDataset.xml");
+        executeDataSet("globalPropertiesTestDataset.xml");
+        executeDataSet("fromMirebalaisMetadataModule.xml");
+        authenticate();
+        activator = new MirebalaisHospitalActivator();
+        activator.started();
+        customProperties = new MirebalaisCustomProperties();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        runtimeProperties = null;
+    }
+
+    @Test
+    @DirtiesContext
+    public void testThatActivatorDoesAllSetup() throws Exception {
+        MirebalaisHospitalService service = Context.getService(MirebalaisHospitalService.class);
+
+        IdentifierPool localZlIdentifierPool = service.getLocalZlIdentifierPool();
+        RemoteIdentifierSource remoteZlIdentifierSource = service.getRemoteZlIdentifierSource();
+        SequentialIdentifierGenerator dossierSequenceGenerator = service.getDossierSequenceGenerator();
+
+        PatientIdentifierType zlIdentifierType = Context.getPatientService().getPatientIdentifierTypeByUuid(
+                MirebalaisConstants.ZL_IDENTIFIER_TYPE_UUID);
+
+        PatientIdentifierType dossierNumberIdentifierType = Context.getPatientService().getPatientIdentifierTypeByUuid(
+                MirebalaisConstants.DOSSIER_NUMBER_IDENTIFIER_TYPE_UUID);
+
+        AutoGenerationOption autoGenerationOption = Context.getService(IdentifierSourceService.class)
+                .getAutoGenerationOption(zlIdentifierType);
+
+        assertEquals(MirebalaisConstants.ZL_IDENTIFIER_TYPE_UUID, zlIdentifierType.getUuid());
+        assertEquals(zlIdentifierType, autoGenerationOption.getIdentifierType());
+        assertEquals(localZlIdentifierPool, autoGenerationOption.getSource());
+
+        assertEquals(MirebalaisConstants.LOCAL_ZL_IDENTIFIER_POOL_UUID, localZlIdentifierPool.getUuid());
+        assertEquals(MirebalaisConstants.LOCAL_ZL_IDENTIFIER_POOL_BATCH_SIZE, localZlIdentifierPool.getBatchSize());
+        assertEquals(MirebalaisConstants.LOCAL_ZL_IDENTIFIER_POOL_MIN_POOL_SIZE, localZlIdentifierPool.getMinPoolSize());
+
+        assertEquals(MirebalaisConstants.REMOTE_ZL_IDENTIFIER_SOURCE_UUID, remoteZlIdentifierSource.getUuid());
+        assertEquals(customProperties.getRemoteZlIdentifierSourceUrl(), remoteZlIdentifierSource.getUrl());
+        assertEquals(customProperties.getRemoteZlIdentifierSourceUsername(), remoteZlIdentifierSource.getUser());
+        assertEquals(customProperties.getRemoteZlIdentifierSourcePassword(), remoteZlIdentifierSource.getPassword());
+
+        assertEquals("A", dossierSequenceGenerator.getPrefix());
+        assertEquals(new Integer(7), dossierSequenceGenerator.getLength());
+        assertEquals("0123456789", dossierSequenceGenerator.getBaseCharacterSet());
+        assertEquals("000001", dossierSequenceGenerator.getFirstIdentifierBase());
+        assertEquals(MirebalaisConstants.DOSSIER_NUMBER_ZL_IDENTIFIER_SOURCE_UUID, dossierSequenceGenerator.getUuid());
+        assertEquals(dossierNumberIdentifierType, dossierSequenceGenerator.getIdentifierType());
 
         assertNotNull(Context.getService(ImportPatientFromWebService.class).getRemoteServers().get("lacolline"));
-	}
-	
+    }
+
 }
