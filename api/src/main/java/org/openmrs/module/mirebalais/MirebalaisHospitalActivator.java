64bb9baf80c3da1d44f403a758416ac5414458db
mogoodrich <mgoodrich@pih.org>
2014-03-26 16:08:48 -0400

    added global property to reference creatinine level concept

--- api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
+++ api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
@@ -304,6 +304,7 @@ public class MirebalaisHospitalActivator implements ModuleActivator {
         setExistingGlobalProperty(RadiologyConstants.GP_LEAD_RADIOLOGY_TECH_NAME, "Edner");
         setExistingGlobalProperty(RadiologyConstants.GP_LEAD_RADIOLOGY_TECH_CONTACT_INFO, "4868-9765");
         setExistingGlobalProperty(RadiologyConstants.GP_CONTRAST_ORDERABLES_CONCEPT, "60e6cd5a-3070-4b41-a36d-4730d4e85733");
+        setExistingGlobalProperty(RadiologyConstants.GP_CREATININE_LEVEL_CONCEPT, "668cd2a5-60dd-4dc4-889b-e09f072c6a1a");
     }
 
     private void setupNamePhoneticsGlobalProperties() {
