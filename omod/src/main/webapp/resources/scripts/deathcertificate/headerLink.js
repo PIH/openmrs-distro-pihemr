3069adfa6238507fb375fec84c8feedb07b9a5c1
Darius Jazayeri <jazayeri@alum.mit.edu>
2014-08-06 16:16:27 -0700

    UHM-1527 - Death Note should display on the patient dashboard

---
+++ omod/src/main/webapp/resources/scripts/deathcertificate/headerLink.js
@@ -0,0 +1,42 @@
+angular.module('deathCertificate', ['encounterService']).
+    controller('DeathCertificateCtrl', ['$scope', 'EncounterService', function ($scope, EncounterService) {
+
+        $scope.patientUuid = null;
+        $scope.existing = { loading: true };
+
+        $scope.init = function (patientUuid) {
+            $scope.patientUuid = patientUuid;
+            EncounterService.getEncounters({ patient: patientUuid, encounterType: "1545d7ff-60f1-485e-9c95-5740b8e6634b" }).
+                then(function (results) {
+                    if (results.length > 0) {
+                        $scope.existing = results[0];
+                    } else {
+                        $scope.existing = null;
+                    }
+                });
+        }
+
+        $scope.fillOutDeathCertificate = function() {
+            emr.navigateTo({
+                provider: "htmlformentryui",
+                page: "htmlform/enterHtmlFormWithStandardUi",
+                query: {
+                    patientId: $scope.patientUuid,
+                    definitionUiResource: "mirebalais:htmlforms/deathCertificate.xml",
+                    returnUrl: emr.pageLink("coreapps", "patientdashboard/patientDashboard", { patientId: $scope.patientUuid })
+                }
+            });
+        }
+
+        $scope.editDeathCertificate = function(encounterUuid) {
+            emr.navigateTo({
+                provider: "htmlformentryui",
+                page: "htmlform/editHtmlFormWithStandardUi",
+                query: {
+                    encounterId: encounterUuid,
+                    patientId: $scope.patientUuid,
+                    returnUrl: emr.pageLink("coreapps", "patientdashboard/patientDashboard", { patientId: $scope.patientUuid })
+                }
+            });
+        }
+    }]);
