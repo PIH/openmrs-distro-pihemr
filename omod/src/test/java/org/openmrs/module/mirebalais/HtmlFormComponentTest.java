973b9090f45215b31c9c05f07f251c2f44a897b9
Darius Jazayeri <jazayeri@alum.mit.edu>
2015-04-02 11:30:19 -0700

    UHM-1838 - Test to verify that all concepts for the death certificate form are present

---
+++ omod/src/test/java/org/openmrs/module/mirebalais/HtmlFormComponentTest.java
@@ -0,0 +1,81 @@
+package org.openmrs.module.mirebalais;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.emrapi.account.AccountService;
+import org.openmrs.module.htmlformentry.FormEntryContext;
+import org.openmrs.module.htmlformentry.FormEntrySession;
+import org.openmrs.module.htmlformentry.HtmlForm;
+import org.openmrs.module.htmlformentry.HtmlFormEntryService;
+import org.openmrs.module.htmlformentryui.HtmlFormUtil;
+import org.openmrs.module.mirebalais.setup.HtmlFormSetup;
+import org.openmrs.module.mirebalaismetadata.MetadataManager;
+import org.openmrs.module.mirebalaismetadata.deploy.bundle.MirebalaisMetadataBundle;
+import org.openmrs.module.paperrecord.PaperRecordProperties;
+import org.openmrs.module.reporting.dataset.definition.service.DataSetDefinitionService;
+import org.openmrs.scheduler.SchedulerService;
+import org.openmrs.test.SkipBaseSetup;
+import org.openmrs.web.test.BaseModuleWebContextSensitiveTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpSession;
+
+import java.io.InputStream;
+
+import static org.junit.Assert.assertTrue;
+
+@SkipBaseSetup
+public class HtmlFormComponentTest extends BaseModuleWebContextSensitiveTest {
+
+    @Autowired
+    private AccountService accountService;
+
+    @Autowired
+    private SchedulerService schedulerService;
+
+    @Autowired
+    private DataSetDefinitionService dataSetDefinitionService;
+
+    @Autowired
+    private PaperRecordProperties paperRecordProperties;
+
+    private MirebalaisHospitalActivator activator;
+
+    @Before
+    public void beforeEachTest() throws Exception {
+        initializeInMemoryDatabase();
+        executeDataSet("requiredDataTestDataset.xml");
+        authenticate();
+        installRequiredMetadata();
+        HtmlFormSetup.setupHtmlFormEntryTagHandlers();
+    }
+
+    @Test
+    public void testHtmlForms() throws Exception {
+        String[] formsToTest = {
+                "deathCertificate.xml"
+        };
+
+        for (String formName : formsToTest) {
+            // loading up all of the UI Framework beans to fetch this resource is problematic, so just get it directly:
+            InputStream in = getClass().getClassLoader().getResourceAsStream("web/module/resources/htmlforms/" + formName);
+            String xml = IOUtils.toString(in);
+            IOUtils.closeQuietly(in);
+            HtmlForm form = HtmlFormUtil.getHtmlFormFromResourceXml(Context.getFormService(), Context.getService(HtmlFormEntryService.class), xml);
+
+            FormEntrySession fes = new FormEntrySession(new Patient(), form, FormEntryContext.Mode.ENTER, new MockHttpSession());
+            String html = fes.getHtmlToDisplay();
+
+            assertTrue("Problem with " + formName, html.indexOf("<htmlform ") >= 0);
+        }
+    }
+
+    private void installRequiredMetadata() {
+        System.setProperty(MirebalaisMetadataBundle.SYSTEM_PROPERTY_SKIP_METADATA_SHARING_PACKAGE_REFRESH, "true");
+        MetadataManager manager = Context.getRegisteredComponents(MetadataManager.class).get(0);
+        manager.refresh();
+    }
+
+}
