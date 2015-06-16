ffd0ce5402654d0e9b3a5c1c486300267ab483d1
mogoodrich <mgoodrich@pih.org>
2015-06-16 11:30:15 -0400

    UHM-2026: Start to use new name template in Mirebalais

--- api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
+++ api/src/main/java/org/openmrs/module/mirebalais/MirebalaisHospitalActivator.java
@@ -22,7 +22,6 @@ import org.openmrs.api.LocationService;
 import org.openmrs.api.UserService;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.db.SerializedObjectDAO;
-import org.openmrs.layout.web.name.NameSupport;
 import org.openmrs.module.Module;
 import org.openmrs.module.ModuleActivator;
 import org.openmrs.module.ModuleFactory;
@@ -36,7 +35,6 @@ import org.openmrs.module.mirebalais.apploader.CustomAppLoaderFactory;
 import org.openmrs.module.mirebalais.setup.AppointmentSchedulingSetup;
 import org.openmrs.module.mirebalais.setup.ArchivesSetup;
 import org.openmrs.module.mirebalais.setup.LegacyMasterPatientIndexSetup;
-import org.openmrs.module.mirebalais.setup.NameTemplateSetup;
 import org.openmrs.module.mirebalais.setup.PrinterSetup;
 import org.openmrs.module.mirebalais.setup.ReportSetup;
 import org.openmrs.module.paperrecord.PaperRecordProperties;
@@ -81,15 +79,6 @@ public class MirebalaisHospitalActivator implements ModuleActivator {
             // Reload configuration based on runtime properties values, defaulting to mirebalais if nothing found
             String configs = Context.getRuntimeProperties().getProperty(ConfigLoader.PIH_CONFIGURATION_RUNTIME_PROPERTY, "mirebalais");
             config.reload(ConfigLoader.load(configs));
-
-            // configure name template (don't do this in Mirebalais yet)
-           if (!config.getSite().equals(ConfigDescriptor.Site.MIREBALAIS)) {
-                NameSupport nameSupport = Context.getRegisteredComponent("nameSupport", NameSupport.class);
-
-                // hack: configure both name support beans, since two actually exist (?)
-                NameTemplateSetup.configureNameTemplate(nameSupport);
-                NameTemplateSetup.configureNameTemplate(NameSupport.getInstance());
-            }
 
             log.info("Mirebalais Hospital Module refreshed");
         }
