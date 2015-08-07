8962df50f57fd43b848e2be9365f9b196661b5a0
lnball <eball@pih.org>
2015-08-07 17:16:18 -0400

    UHM-x Added NCD consult form to the API.

--- api/src/main/java/org/openmrs/module/mirebalais/apploader/CustomAppLoaderConstants.java
+++ api/src/main/java/org/openmrs/module/mirebalais/apploader/CustomAppLoaderConstants.java
@@ -19,6 +19,7 @@ public class CustomAppLoaderConstants {
         public static final String DISPENSING = "dispensing";
         public static final String SURGERY = "surgery";
         public static final String ONCOLOGY = "oncology";
+        public static final String NCD = "ncd";
         public static final String LAB_RESULTS = "labResults";
         public static final String OVERVIEW_REPORTS = "overviewReports";
         public static final String DATA_EXPORTS = "dataExports";
@@ -94,6 +95,7 @@ public class CustomAppLoaderConstants {
         public static final String SURGICAL_NOTE_VISIT_ACTION = "pih.form.surgicalNote";
         public static final String ONCOLOGY_CONSULT_NOTE_VISIT_ACTION = "pih.form.oncologyNote";
         public static final String LAB_RESULTS_VISIT_ACTION = "pih.form.labResults";
+        public static final String NCD_VISIT_ACTION = "pih.form.ncdConsult";
         public static final String ORDER_XRAY_VISIT_ACTION = "radiologyapp.orderXray";
         public static final String ORDER_CT_VISIT_ACTION = "radiologyapp.orderCT";
         public static final String ORDER_ULTRASOUND_VISIT_ACTION = "radiologyapp.orderUS";
@@ -237,6 +239,7 @@ public class CustomAppLoaderConstants {
             Extensions.SURGICAL_NOTE_VISIT_ACTION,
             Extensions.ONCOLOGY_CONSULT_NOTE_VISIT_ACTION,
             Extensions.LAB_RESULTS_VISIT_ACTION,
+            Extensions.NCD_VISIT_ACTION,
             Extensions.ORDER_XRAY_VISIT_ACTION,
             Extensions.ORDER_CT_VISIT_ACTION,
             Extensions.ORDER_ULTRASOUND_VISIT_ACTION);
