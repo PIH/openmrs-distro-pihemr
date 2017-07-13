96102c9d5601cc4622838152da8ac784b9f48c8d
mgoodrich <mark.goodrich@live.com>
2017-07-13 09:35:35 -0400

    update pihemr deploy to include  -U

--- pihemrDeploy.sh
+++ pihemrDeploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git pull
-mvn openmrs-sdk:deploy -Ddistro=api/src/main/resources/openmrs-distro.properties
+mvn openmrs-sdk:deploy -Ddistro=api/src/main/resources/openmrs-distro.properties -U
