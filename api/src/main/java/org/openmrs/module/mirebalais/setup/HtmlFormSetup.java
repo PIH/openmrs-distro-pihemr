4a1d07b5cd95f93499d2d6b0e6c1443f70dfd179
mgoodrich <mark.goodrich@live.com>
2018-01-22 14:59:28 -0500

    refactor location of some htmlforms out of Haiti-specific directory

--- api/src/main/java/org/openmrs/module/mirebalais/setup/HtmlFormSetup.java
+++ api/src/main/java/org/openmrs/module/mirebalais/setup/HtmlFormSetup.java
@@ -10,6 +10,7 @@ import org.openmrs.module.pihcore.config.Config;
 import org.openmrs.module.pihcore.config.ConfigDescriptor;
 import org.openmrs.ui.framework.resource.ResourceFactory;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,81 +25,80 @@ public class HtmlFormSetup {
             FormService formService = Context.getFormService();
             HtmlFormEntryService htmlFormEntryService = Context.getService(HtmlFormEntryService.class);
 
-            List<String> htmlforms = null;
+            // forms installed across all implementations
+            List<String> htmlforms = new ArrayList(Arrays.asList(
+                "pihcore:htmlforms/admissionNote.xml",
+                "pihcore:htmlforms/patientRegistration.xml",
+                "pihcore:htmlforms/patientRegistration-rs.xml",
+                "pihcore:htmlforms/surgicalPostOpNote.xml",
+                "pihcore:htmlforms/vitals.xml",
+                "pihcore:htmlforms/transferNote.xml",
+                "pihcore:htmlforms/dischargeNote.xml",
+                "pihcore:htmlforms/outpatientConsult.xml",
+                "pihcore:htmlforms/edNote.xml",
+                "pihcore:htmlforms/deathCertificate.xml",
+                "pihcore:htmlforms/oncologyConsult.xml",
+                "pihcore:htmlforms/mentalHealth.xml",
+                "pihcore:htmlforms/section-chief-complaint.xml",
+                "pihcore:htmlforms/section-history.xml",
+                "pihcore:htmlforms/section-exam.xml",
+                "pihcore:htmlforms/section-dx.xml",
+                "pihcore:htmlforms/section-peds-feeding.xml",
+                "pihcore:htmlforms/section-peds-supplements.xml",
+                "pihcore:htmlforms/section-plan.xml",
+                "pihcore:htmlforms/section-ncd.xml",
+                "pihcore:htmlforms/section-lab-order.xml",
+                "pihcore:htmlforms/section-family-planning.xml",
+                "pihcore:htmlforms/section-socio-econ.xml",
+                "pihcore:htmlforms/primary-care-adult-initial.xml",
+                "pihcore:htmlforms/primary-care-adult-followup.xml",
+                "pihcore:htmlforms/primary-care-peds-initial.xml",
+                "pihcore:htmlforms/primary-care-peds-followup.xml",
+                "pihcore:htmlforms/ncd-adult-initial.xml",
+                "pihcore:htmlforms/ncd-adult-followup.xml"));
 
+            // add any country-specific forms
             if (config.getCountry().equals(ConfigDescriptor.Country.HAITI)) {
-                htmlforms = Arrays.asList("pihcore:htmlforms/admissionNote.xml",
-                        "pihcore:htmlforms/patientRegistration.xml",
-                        "pihcore:htmlforms/patientRegistration-rs.xml",
-                        "pihcore:htmlforms/surgicalPostOpNote.xml",
-                        "pihcore:htmlforms/vitals.xml",
-                        "pihcore:htmlforms/transferNote.xml",
-                        "pihcore:htmlforms/dischargeNote.xml",
-                        "pihcore:htmlforms/outpatientConsult.xml",
-                        "pihcore:htmlforms/edNote.xml",
-                        "pihcore:htmlforms/deathCertificate.xml",
-                        "pihcore:htmlforms/haiti/section-chief-complaint.xml",
-                        "pihcore:htmlforms/haiti/section-history.xml",
-                        "pihcore:htmlforms/haiti/section-exam.xml",
-                        "pihcore:htmlforms/haiti/section-dx.xml",
-                        "pihcore:htmlforms/haiti/section-peds-feeding.xml",
-                        "pihcore:htmlforms/haiti/section-peds-supplements.xml",
-                        "pihcore:htmlforms/haiti/section-plan.xml",
-                        "pihcore:htmlforms/haiti/section-ncd.xml",
-                        "pihcore:htmlforms/haiti/primary-care-adult-initial.xml",
-                        "pihcore:htmlforms/haiti/primary-care-adult-followup.xml",
-                        "pihcore:htmlforms/haiti/primary-care-peds-initial.xml",
-                        "pihcore:htmlforms/haiti/primary-care-peds-followup.xml",
-                        "pihcore:htmlforms/haiti/ncd-adult-initial.xml",
-                        "pihcore:htmlforms/haiti/ncd-adult-followup.xml",
+                htmlforms.addAll(Arrays.asList(
+                        "pihcore:htmlforms/haiti/hiv/zl/hiv-intake.xml",
                         "pihcore:htmlforms/haiti/patientRegistration-contact.xml",
                         "pihcore:htmlforms/haiti/patientRegistration-social.xml",
                         "pihcore:htmlforms/haiti/patientRegistration-insurance.xml",
                         "pihcore:htmlforms/haiti/checkin.xml",
                         "pihcore:htmlforms/haiti/liveCheckin.xml",
-                        "pihcore:htmlforms/oncologyConsult.xml",
-                        "pihcore:htmlforms/mentalHealth.xml",
-                        "pihcore:htmlforms/haiti/hiv/zl/hiv-intake.xml",
                         "pihcore:htmlforms/haiti/hiv/zl/hiv-followup.xml",
                         "pihcore:htmlforms/haiti/hiv/zl/vct.xml",
-                        // "pihcore:htmlforms/haiti/hiv/zl/hiv-child-intake.xml",
-                        // "pihcore:htmlforms/haiti/hiv/zl/hiv-child-followup.xml",
                         "pihcore:htmlforms/haiti/hiv/zl/section-hiv-serology.xml",
                         "pihcore:htmlforms/haiti/hiv/zl/section-risk-factors.xml",
                         "pihcore:htmlforms/haiti/hiv/zl/section-symptoms.xml",
                         "pihcore:htmlforms/haiti/hiv/zl/section-who-stages.xml",
                         "pihcore:htmlforms/haiti/hiv/zl/section-hiv-oi.xml",
                         "pihcore:htmlforms/haiti/hiv/zl/section-hiv-state.xml",
-                        "pihcore:htmlforms/section-lab-order.xml",
-                        "pihcore:htmlforms/section-family-planning.xml",
-                        "pihcore:htmlforms/haiti/section-socio-econ.xml",
                         "pihcore:htmlforms/haiti/hiv/iSantePlus/Adherence.xml",
                         "pihcore:htmlforms/haiti/hiv/iSantePlus/SaisiePremiereVisiteAdult.xml",
                         "pihcore:htmlforms/haiti/hiv/iSantePlus/SaisiePremiereVisitePediatrique.xml",
                         "pihcore:htmlforms/haiti/hiv/iSantePlus/VisiteDeSuivi.xml",
                         "pihcore:htmlforms/haiti/hiv/iSantePlus/VisiteDeSuiviPediatrique.xml"
-                );
+                ));
             }
             else if (config.getCountry().equals(ConfigDescriptor.Country.LIBERIA)) {
-                htmlforms = Arrays.asList(
-                        "pihcore:htmlforms/patientRegistration.xml",
-                        "pihcore:htmlforms/patientRegistration-rs.xml",
+                htmlforms.addAll(Arrays.asList(
                         "pihcore:htmlforms/liberia/checkin.xml",
                         "pihcore:htmlforms/liberia/liveCheckin.xml",
                         "pihcore:htmlforms/liberia/patientRegistration-contact.xml",
                         "pihcore:htmlforms/liberia/patientRegistration-social.xml"
-                );
+                ));
             }
 
             else if (config.getCountry().equals(ConfigDescriptor.Country.SIERRA_LEONE)) {
-                htmlforms = Arrays.asList(
+                htmlforms.addAll(Arrays.asList(
                         "pihcore:htmlforms/patientRegistration.xml",
                         "pihcore:htmlforms/patientRegistration-rs.xml",
                         "pihcore:htmlforms/sierra_leone/checkin.xml",
                         "pihcore:htmlforms/sierra_leone/liveCheckin.xml",
                         "pihcore:htmlforms/sierra_leone/patientRegistration-contact.xml",
                         "pihcore:htmlforms/sierra_leone/patientRegistration-social.xml"
-                );
+                ));
             }
 
             if (htmlforms != null) {
