3bf908867a77cc215aee8079ed21a54af326fe95
Amos Laboso <alaboso@pih.org>
2019-11-05 11:43:56 +0000

    LIB - Epilepsy section within the Mental Health form; removed 'include' feature to be implemented within HFE

--- omod/src/main/webapp/resources/scripts/liberia.js
+++ omod/src/main/webapp/resources/scripts/liberia.js
@@ -1,10 +1,18 @@
-$( document ).ready(function() {
+var omrs = {
+    toggleExposureDurationField: function (x, containerId) {
+        if (x == 1) {
+            $('#' + containerId).show();
+        } else if (x == 0) {
+            $('#' + containerId).hide();
+        }
+    }
+}
 
-    $(".icon-arrow-down").siblings('div').hide();
+$(document).ready(function () {
 
     $(".icon-arrow-down").click(function () {
         var self = $(this);
-        if(self.siblings('div').is(":visible")){
+        if (self.siblings('div').is(":visible")) {
             self.siblings("div").hide("fast", function () {
                 self.removeClass('icon-arrow-up').addClass('icon-arrow-down');
             });
