b2431c94cb95e8850a2a3fa39fcacc7d51a44c19
mgoodrich <mark.goodrich@live.com>
2020-03-02 15:09:33 -0500

    anothe Hail Mary attempt to re-enable component tests

--- api/src/test/java/org/openmrs/module/mirebalais/radiology/RadiologyOrderNumberGeneratorComponentTest.java
+++ api/src/test/java/org/openmrs/module/mirebalais/radiology/RadiologyOrderNumberGeneratorComponentTest.java
@@ -2,7 +2,6 @@ package org.openmrs.module.mirebalais.radiology;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.openmrs.Order;
 import org.openmrs.TestOrder;
@@ -25,7 +24,6 @@ import static org.junit.Assert.assertThat;
 
 ;
 
-@Ignore
 public class RadiologyOrderNumberGeneratorComponentTest extends BaseModuleContextSensitiveTest {
 
     @Autowired
