886f9a0edcfd6c8c97adb7a841f84236434b893c
Brandon Istenes <bistenes@gmail.com>
2021-05-06 21:58:00 -0700

    Add PIH microfrontends to distro

--- distro/build-frontend.sh
+++ distro/build-frontend.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
 set +x
-npx --yes "openmrs@${APP_SHELL_VERSION}" build --spa-path "${SPA_PATH}" --api-url "${API_URL}" --target "$1"
-npx --yes "openmrs@${APP_SHELL_VERSION}" assemble --mode config --target "$1"
+npx --yes openmrs@${APP_SHELL_VERSION} build --spa-path \${SPA_PATH} --api-url \${API_URL} --target "$1"
+npx --yes openmrs@${APP_SHELL_VERSION} assemble --mode config --target "$1"
