7e6e41ca5b7749283b26f753ed4e7458f7a9992e
Brandon Istenes <bistenes@gmail.com>
2021-05-07 09:44:30 -0700

    Add site config to import map

--- distro/build-frontend.sh
+++ distro/build-frontend.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 set +x
 npx --yes openmrs@${APP_SHELL_VERSION} build --spa-path \${SPA_PATH} --api-url \${API_URL} --target "$1"
 npx --yes openmrs@${APP_SHELL_VERSION} assemble --mode config --target "$1"
+npx json-merger --pretty "$1/importmap.json" importmap-overrides.json --output "$1/importmap.json.tmp"
+mv "$1/importmap.json.tmp" "$1/importmap.json"
