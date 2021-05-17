ed7b83ed1ce0a9806ca879d3eb6aadb0cb643cd5
Michael Seaton <mseaton@pih.org>
2021-05-17 15:09:16 -0400

    UHM-5522:  Remove custom Haiti metadata to config-zl (#158)

--- api/src/main/java/org/openmrs/module/mirebalais/setup/LegacyMasterPatientIndexSetup.java
+++ api/src/main/java/org/openmrs/module/mirebalais/setup/LegacyMasterPatientIndexSetup.java
@@ -12,7 +12,7 @@ import org.openmrs.module.importpatientfromws.api.RemoteServerConfiguration;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.module.mirebalais.MirebalaisConstants;
 import org.openmrs.module.mirebalais.RuntimeProperties;
-import org.openmrs.module.pihcore.metadata.haiti.PihHaitiPatientIdentifierTypes;
+import org.openmrs.module.pihcore.ZlConfigConstants;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -32,9 +32,9 @@ public class LegacyMasterPatientIndexSetup {
         }
 
         Map<String, PatientIdentifierType> identifierTypeMap = new HashMap<String, PatientIdentifierType>();
-        identifierTypeMap.put("a541af1e-105c-40bf-b345-ba1fd6a59b85", MetadataUtils.existing(PatientIdentifierType.class, PihHaitiPatientIdentifierTypes.ZL_EMR_ID.uuid()));
+        identifierTypeMap.put("a541af1e-105c-40bf-b345-ba1fd6a59b85", MetadataUtils.existing(PatientIdentifierType.class, ZlConfigConstants.PATIENTIDENTIFIERTYPE_ZLEMRID_UUID));
         // TODO create PatientIdentifierType for Lacolline KE dossier number
-        identifierTypeMap.put("e66645eb-03a8-4991-b4ce-e87318e37566", MetadataUtils.existing(PatientIdentifierType.class, PihHaitiPatientIdentifierTypes.EXTERNAL_DOSSIER_NUMBER.uuid()));
+        identifierTypeMap.put("e66645eb-03a8-4991-b4ce-e87318e37566", MetadataUtils.existing(PatientIdentifierType.class, ZlConfigConstants.PATIENTIDENTIFIERTYPE_EXTERNALDOSSIERNUMBER_UUID));
         // TODO create PatientIdentifierType for Lacolline dental dossier number
 
         Map<String, Location> locationMap = new HashMap<String, Location>();
