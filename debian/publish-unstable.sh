f3bf203cc6cb97400b5f41ccfad47ca5fc9c44c9
Michael Seaton <mseaton@pih.org>
2022-07-11 15:45:34 -0400

    UHM-4548 - Add initial script to publish unstable debian package to jfrog

--- debian/publish-unstable.sh
+++ debian/publish-unstable.sh
@@ -6,7 +6,9 @@
 
 DEBIAN_REPO_BASE_URL="https://openmrs.jfrog.io/artifactory/deb-pih/pool"
 DEBIAN_REPO_DISTRIBUTION=unstable
-DEBIAN_FILENAME="$(ls ${project.build.directory}/debian/*.deb)"
+
+cd ${project.build.directory}/debian/
+DEBIAN_FILENAME="$(ls *.deb)"
 source $HOME/.pihemr-debian-env
 
 curl -u${DEBIAN_REPO_USER}:${DEBIAN_REPO_PASSWORD} -X PUT "${DEBIAN_REPO_BASE_URL}/${DEBIAN_FILENAME};deb.distribution=${DEBIAN_REPO_DISTRIBUTION};deb.component=main;deb.architecture=amd64" -T ${DEBIAN_FILENAME}
