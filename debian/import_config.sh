993ea609eb2d5774d6323e5708783a9b35dd2b2b
Andy Cotton <acotton@pih.org>
2024-10-31 16:02:11 -0400

    Use openmrs-core, multi country build

--- debian/import_config.sh
+++ debian/import_config.sh
@@ -1,26 +1,58 @@
 #!/bin/bash
 
-CONFIG=${1:-"openmrs-config-pihsl"}
-FRONTEND=${2:-"openmrs-frontend-pihemr"}
-
-echo "configuration is $CONFIG"
-echo "frontend is $FRONTEND"
-
 #Create dirs for unzipping resources
+rm -rf ./target/frontend
 mkdir ./target/frontend
+rm -rf ./target/frontend_zl
+mkdir ./target/frontend_zl
+rm -rf ./target/configuration
 mkdir ./target/configuration
+rm -rf ./target/configuration_zl
+mkdir ./target/configuration_zl
+rm -rf ./target/configuration_pihsl
+mkdir ./target/configuration_pihsl
+rm -rf ./target/configuration_ces
+mkdir ./target/configuration_ces
+rm -rf ./target/configuration_pihliberia
+mkdir ./target/configuration_pihliberia
 
 #retrive config and frontend from maven
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
--Dartifact=org.pih.openmrs:"$CONFIG":LATEST:zip \
--DoutputDirectory=./ -Dmdep.useBaseVersion=true
+-Dartifact=org.pih.openmrs:openmrs-frontend-pihemr:LATEST:zip \
+-DoutputDirectory=./target/ -Dmdep.useBaseVersion=true
 
 mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
--Dartifact=org.pih.openmrs:"$FRONTEND":LATEST:zip \
--DoutputDirectory=./ -Dmdep.useBaseVersion=true
+-Dartifact=org.pih.openmrs:openmrs-frontend-zl:LATEST:zip \
+-DoutputDirectory=./target/ -Dmdep.useBaseVersion=true
+
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
+-Dartifact=org.pih.openmrs:openmrs-config-pihemr:LATEST:zip \
+-DoutputDirectory=./target/ -Dmdep.useBaseVersion=true
+
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
+-Dartifact=org.pih.openmrs:openmrs-config-zl:LATEST:zip \
+-DoutputDirectory=./target/ -Dmdep.useBaseVersion=true
+
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
+-Dartifact=org.pih.openmrs:openmrs-config-pihsl:LATEST:zip \
+-DoutputDirectory=./target/ -Dmdep.useBaseVersion=true
+
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
+-Dartifact=org.pih.openmrs:openmrs-config-ces:LATEST:zip \
+-DoutputDirectory=./target/ -Dmdep.useBaseVersion=true
+
+mvn org.apache.maven.plugins:maven-dependency-plugin:2.8:copy \
+-Dartifact=org.pih.openmrs:openmrs-config-pihliberia:LATEST:zip \
+-DoutputDirectory=./target/ -Dmdep.useBaseVersion=true
 
 #unzip maven resources
 
-unzip -o "$CONFIG"* -d ./target/configuration/
-unzip -o "$FRONTEND"* -d ./target/frontend/
+
+unzip -o ./target/"openmrs-frontend-pihemr"* -d ./target/frontend/
+unzip -o ./target/"openmrs-frontend-zl"* -d ./target/frontend_zl/
+unzip -o ./target/"openmrs-config-pihemr"* -d ./target/configuration/
+unzip -o ./target/"openmrs-config-zl"* -d ./target/configuration_zl/
+unzip -o ./target/"openmrs-config-pihsl"* -d ./target/configuration_pihsl/
+unzip -o ./target/"openmrs-config-ces"* -d ./target/configuration_ces/
+unzip -o ./target/"openmrs-config-pihliberia"* -d ./target/configuration_pihliberia/
